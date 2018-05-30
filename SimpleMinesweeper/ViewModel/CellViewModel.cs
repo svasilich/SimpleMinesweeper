540891ed35e8563558543b6fdba7b609e38e7606
Fedyk Stepan <svasilich@gmail.com>
2018-05-30 15:53:42 +0300

    Не показываем нулевое количество мин вокруг.

--- SimpleMinesweeper/ViewModel/CellViewModel.cs
+++ SimpleMinesweeper/ViewModel/CellViewModel.cs
@@ -173,4 +173,22 @@ namespace SimpleMinesweeper.ViewModel
             return 0;
         }
     }
+
+    public class NearbyTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int nearby = (int)value;
+
+            if (nearby > 0 && nearby <= 8)
+                return nearby;
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
