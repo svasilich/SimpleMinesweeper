5c480efabafe2a3c242b2b616edea600c5dcb7ab
Fedyk Stepan <svasilich@gmail.com>
2018-06-05 23:14:26 +0300

    Функциональность игрового таймера.

--- SimpleMinesweeper/ViewModel/MinefieldViewModel.cs
+++ SimpleMinesweeper/ViewModel/MinefieldViewModel.cs
@@ -8,7 +8,9 @@ using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using System.Windows;
+using System.Windows.Data;
 using SimpleMinesweeper.Core;
+using System.Globalization;
 
 namespace SimpleMinesweeper.ViewModel
 {
@@ -40,6 +42,7 @@ namespace SimpleMinesweeper.ViewModel
 
                     case FieldState.NotStarted:
                         gameTimer = null;
+                        GameTime = 0;
                         break;
 
                     default:
@@ -235,6 +238,25 @@ namespace SimpleMinesweeper.ViewModel
         public void Execute(object parameter)
         {
             field.Fill(height, width, mineCount);
+        }
+    }
+
+    public class TimerSecondsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int tiks = (int)value;
+            if (tiks >= 60000) // 100 * 60
+                tiks = 999 * 60 + 59; // "999:59"
+
+            int seconds = tiks % 60;
+            int minutes = tiks / 60;
+            return string.Format("{0:d2}:{1:d2}", minutes, seconds);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ SimpleMinesweeperTests/ViewModel/TimerSecundsConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using SimpleMinesweeper.ViewModel;
+
+namespace SimpleMinesweeperTests.ViewModel
+{
+    [TestFixture]
+    class TimerSecundsConverterTests
+    {
+        [TestCase(0, "00:00")]
+        [TestCase(60, "01:00")]
+        [TestCase(5999, "99:59")]
+        [TestCase(6000, "100:00")]
+        [TestCase(60000, "999:59")]
+        public void TestTimeConvertions(int seconds, string expected)
+        {
+            var converter = new TimerSecondsConverter();
+            string result = (string)converter.Convert(seconds, null, null, null);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
