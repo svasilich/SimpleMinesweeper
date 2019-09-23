aa17a548141d315db23fec1b3a856aa51e4d1066
sVasilich <sVasilich@gmail.com>
2019-09-23 20:21:19 +0300

    RecordTimeConverter перенесён из своего пространства имён.

--- SimpleMinesweeper/ViewModel/RecordsViewModelSys/RecordTimeConverter.cs
+++ SimpleMinesweeper/ViewModel/RecordsViewModelSys/RecordTimeConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace SimpleMinesweeper.ViewModel.RecordsViewModelSys
+namespace SimpleMinesweeper.ViewModel
 {
     public class RecordTimeConverter : IValueConverter
     {
